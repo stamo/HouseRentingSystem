e80212c5404316b7449568554fddef43248a8b02
Стамо Петков <s.g.petkov@is-bg.net>
2022-11-23 21:20:42 +0200

    UnitTest project added

---
+++ HouseRentingSystem.UnitTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace HouseRentingSystem.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
